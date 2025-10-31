67ebb3907b558b81cc326d23d1386535eb433f2e
Noc707 <236208980+Noc707@users.noreply.github.com>
2025-10-31 04:06:20 +0000

    function
    added function get computer choice with coices being rock, paper, or
    scissors. then added random idex with math.random to get a random
    result.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log ("Hello world")
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+    
+}
+
+const computersPick = getComputerChoice();
+console.log("The computer chose: " + computersPick);
+
+
+
+
+
+
+
+
+
+
+
+
