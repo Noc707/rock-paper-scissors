62cb9036a86a9540d030cb32d97330175f194dcd
Noc707 <236208980+Noc707@users.noreply.github.com>
2025-11-01 01:59:19 +0000

    arguments
    added else ifs.

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function getComputerChoice() {
     
 }
 
-const computersPick = getComputerChoice();
+    const computersPick = getComputerChoice();
 console.log("The computer chose: " + computersPick);
 
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+    return prompt("Rock, paper, or scissors?").toLowerCase();
 }
 
 console.log("humanChoice");
@@ -20,9 +20,31 @@ console.log("humanChoice");
     let computerChoice = 0;
 
 function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("Its a tie!")
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You loose!")
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("Winner!")
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("Winner!")
+    }
+    else if (humanChoice == "Paper" && computerChoice == "scissors") {
+        console.log("You loose!")
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You loose!")
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("Winner!")
+    }
 }
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
 
     playRound(humanSelection, computerSelection);
+
 
